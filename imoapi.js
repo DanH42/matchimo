bd727441a0a5808c245f21ccad36981083ff88df
Nexus Unplugged <DanH1420@gmail.com>
2013-10-12 03:53:06 -0500

    Lotsa changes

--- imoapi.js
+++ imoapi.js
@@ -1,82 +1,69 @@
-var channel;
-var userName;
-var msg, game, nameForm, nameInput, nameButton, userList;
-var scoreForm, scoreInput, scoreButton;
+window.cssFinalize = false;
 
-// Map user IDs (setters) to names and other data
-var users = {};
-// Array of user IDs to indicate turn order
-var turnOrder = [];
-var currentTurn = 0;
+var channel, myUserList, mySelectionDisabler;
+var msg, game, userList, startButton;
 
-function on_submit_name(){
-	// Check if any name has been entered
-	if($.trim(nameInput.value) !== ""){
-		nameInput.disabled = nameButton.disabled = true;
-		// Send a message announcing the user's arrival to all users
-		channel.event_queue("users", {"object": {"name": nameInput.value}});
-		on_join(nameInput.value);
+// Make this dynamic later, maybe?
+var gridSize = 4;
+var board = [];
+
+function create_board(){
+	var selected = [];
+	for(var i = 0; i < people.length; i++)
+		selected.push(i);
+	selected = shuffle(selected).slice(0, 8);
+
+	var order = [];
+	for(var i = 0; i < selected.length; i++){
+		order.push({i: selected[i], type: "name"});
+		order.push({i: selected[i], type: "photo"});
 	}
-	return false;
+
+	return shuffle(order);
 }
 
-function on_join(name){
-	// Store the user's name for later
-	userName = name;
-	// Yes, eew for many reasons.
-	game.innerHTML = '<form id="score_form">Add Score: <input type=text id="score_input" disabled /><input type="submit" id="score_button" value="Add" disabled /></form>';
-	scoreForm = document.getElementById("score_form");
-	scoreInput = document.getElementById("score_input");
-	scoreButton = document.getElementById("score_button");
-	$(scoreForm).submit(on_submit_score);
+function load_board(order){
+	board = order;
+	for(var i = 0; i < board.length; i++){
+		var opts = {};
+		opts[board[i].type] = true;
+		render_profile(board[i].i, opts, document.getElementsByClassName('profile')[i]);
+	}
+}
+
+function startGame(){
+	var order = create_board();
+	channel.event_queue("board", {"object": {"board": order}});
 }
 
 function id_to_name(id){
-	if(users[id])
-		return users[id].name;
+	var name = myUserList.get_data(id, "name")
+	if(name)
+		return name;
 	return "Someone (" + id + ")";
 }
-
-////////////////////////////////////////////////////////////////////////////////
-
-function on_submit_score(){
-	var points = parseInt(scoreInput.value);
-	// Check if any score has been entered
-	if(points !== NaN){
-		scoreInput.value = "";
-		channel.event_queue("moves", {"object": {"move": {"points": points}}});
-	}
-	return false;
-}
-
-function check_turn(){
-	if(turnOrder[currentTurn] === channel.get_public_client_id())
-		scoreInput.disabled = scoreButton.disabled = false;
-}
-
-////////////////////////////////////////////////////////////////////////////////
 
 // Call this function any time a user's information changes
 function update_users(){
 	userList.style.display = "none";
 	userList.innerHTML = "<tr><th></th><th>Score</th></tr>";
-	for(var i in turnOrder){
-		var user = users[turnOrder[i]];
-		if(!user) continue;
-
+	for(var id in myUserList.users){
 		var tr = document.createElement("tr");
-		if(currentTurn == i)
-			tr.className = "current";
-		else if(turnOrder[i] === channel.get_public_client_id())
-			tr.className = "me";
 
 		var name_td = document.createElement("td");
 		name_td.className = "name";
-		$(name_td).text(user.name);
+		$(name_td).text(myUserList.get_data(id, "name"));
+		var img = myUserList.users[id].icon_url;
+		var color = "255,255,255";
+		if(id === channel.get_public_client_id()){
+			tr.className = "me";
+			color = "181,211,255";
+		}
+		$(name_td).css("background", "linear-gradient(to right, rgba(" + color + ",0) 0%,rgba(" + color + ",1) 100%) repeat-y, url(" + img + ") no-repeat");
 		tr.appendChild(name_td);
 
 		var score_td = document.createElement("td");
-		$(score_td).text(user.score);
+		$(score_td).text(myUserList.get_data(id, "score"));
 		tr.appendChild(score_td);
 
 		userList.appendChild(tr);
@@ -84,57 +71,59 @@ function update_users(){
 	userList.style.display = "block";
 }
 
+// http://stackoverflow.com/a/6274381/802335
+function shuffle(o){
+	for(var j, x, i = o.length; i; j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x);
+	return o;
+}
+
 function connect(){
 	var client = {
 		connect: function(){
-			// Enable name entry
-			nameInput.disabled = nameButton.disabled = false;
-			nameInput.focus();
-			msg.innerHTML = "Please choose a name.";
-
-			// Subscribe to event queues to receive all past and future messages
-			channel.subscribe([{type: "event_queue", name: "users"}], 0);
+			msg.innerHTML = "Connected.";
+			channel.subscribe([{type: "event_queue", name: "imo.clients"}], 0);
+			channel.subscribe([{type: "event_queue", name: "board"}], 0);
 			channel.subscribe([{type: "event_queue", name: "moves"}], 0);
+			myUserList = new IMO.UserList({
+				"title": "Players",
+				"public_client_id": channel.get_public_client_id(),
+				"device": "web",
+				"list_style": "column"
+			});
 		},
 
 		event_queue: function(name, event){
 			console.log([name, event.object]);
-			if((name == "users") && (event.object.name)){
-				if(!users[event.setter]){
-					turnOrder.push(event.setter);
+			if(name === "imo.clients" && event.object.action === "join"){
+				if(!myUserList.users[event.setter]){
+					myUserList.add_user(event);
+					var newName = event.object.first_name;
+					if(newName === "Guest")
+						newName += " " + event.object.last_name;
+
 					// Create a default "player file"
-					users[event.setter] = {
-						name: event.object.name,
-						score: 0
-					};
-				}else
-					users[event.setter].name = event.object.name;
+					myUserList.set_data(event.setter, "name", newName);
+					myUserList.set_data(event.setter, "score", 0);
+				}
 
-				if(event.setter === channel.get_public_client_id())
-					on_join(event.object.name);
-
-				if(turnOrder.length > 1){
+				if(event.setter !== channel.get_public_client_id()){
 					// We have at least 2 users, let's do things!
-					scoreInput.disabled = scoreButton.disabled = false;
-					msg.innerHTML = event.object.name + " has joined";
-				}else
-					msg.innerHTML = "Waiting for another user...";
+					startButton.disabled = false;
+					startButton.value = "Start Game";
+					startButton.onclick = startGame;
+
+					msg.innerHTML = myUserList.get_data(event.setter, "name") + " has joined";
+				}
+
 				update_users();
-			}else if((name == "moves") && (event.object.move)){
-				if(event.setter == turnOrder[currentTurn]){
-					if(event.setter === channel.get_public_client_id())
-						scoreInput.disabled = scoreButton.disabled = true;
-
-					users[event.setter].score += event.object.move.points;
-					msg.innerHTML = id_to_name(event.setter) + " earned " + event.object.move.points + " points!";
-
-					currentTurn++;
-					if(currentTurn >= turnOrder.length)
-						currentTurn = 0;
-					check_turn();
-					update_users();
+            }else if(name === "board" && event.object.board){
+				if(board.length == 0){
+					startButton.disabled = true;
+					load_board(event.object.board);
 				}else
-					console.log(id_to_name(event.setter) + " tried to make a move, but it wasn't their turn!");
+					console.log(id_to_name(event.setter) + " tried to start a game, but you were still playing!");
+			}else if(name === "moves" && event.object.move){
+				// Moves go here
 			}
 		}
 	};
@@ -142,14 +131,15 @@ function connect(){
 };
 
 window.onload = function(){
-	nameForm = document.getElementById("name_form");
-	nameInput = document.getElementById("name_input");
-	nameButton = document.getElementById("name_button");
-	$(nameForm).submit(on_submit_name);
-
 	msg = document.getElementById('msg');
 	game = document.getElementById('game');
 	userList = document.getElementById('userList');
+	startButton = document.getElementById('start');
+	mySelectionDisabler = new IMO.SelectionDisabler();
+	mySelectionDisabler.recursively_disable_selection(game, []);
+	$(".profile").click(function(){
+		$(this).toggleClass("selected");
+	});
 
 	channel = connect();
 	msg.innerHTML = "Connecting...";
